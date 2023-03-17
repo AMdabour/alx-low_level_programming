f7d3feba01a53444b801d069788601fb7f7e6f74
AMdabour <ah910905@gmail.com>
2023-03-17 12:21:28 -0700

    while and if

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - emtry point
+ *
+ * Description: print lowercase letters except q and e.
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		if (letter != 'q' || 'e')
+		{
+			putchar(letter);
+			letter++;
+		}
+		else
+		{
+			letter++;
+		}
+	}
+	return (0);
+}

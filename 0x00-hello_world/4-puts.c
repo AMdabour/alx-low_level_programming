e7660403bcfe8c58b07effd44f865e007093a7ba
AMdabour <ah910905@gmail.com>
2023-03-16 18:24:50 -0700

    edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
-#!/bin/bas
-h
 #include <stdio.h>
+
+/**
+ * main: entry point
+ *
+ * Description: printing with puts function
+ *
+ * Return: always 0 as a successful process
+*/
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

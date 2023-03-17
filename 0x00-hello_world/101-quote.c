87bf715ad176d3a22f4683381af0840c8bc05723
AMdabour <ah910905@gmail.com>
2023-03-16 20:09:34 -0700

    betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,16 @@
 /**
  * main - Entry point of the program
  *
- * Description: This program prints a message followed by the author and date to the standard error.
+ * Description: This program prints a message the write function.
  *
  * Return: Always 1 (to indicate an error)
  */
 
 int main(void)
+
 {
 	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1,msg,57);
+
+	write(1, msg, 59);
 	return (1);
 }

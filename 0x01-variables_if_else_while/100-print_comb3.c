9b28ced043027f919c5974385e9a2250a110484e
AMdabour <ah910905@gmail.com>
2023-03-19 10:31:29 -0700

    two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int d1, d2;
+
+	d1 = 0;
+	while (d1 <= 9)
+	{
+		d2 = 0;
+		while (d2 <= 9)
+		{
+			if (d1 != d2 && d1 < d2)
+			{
+				putchar(d1 + '0');
+				putchar(d2 + '0');
+				if (d1 + d2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			d2++;
+		}
+		d1++;
+	}
+	return (0);
+}

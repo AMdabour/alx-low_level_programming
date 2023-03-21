476f64e533ac7c5974552192b450255e9b56d6cb
AMdabour <ah910905@gmail.com>
2023-03-21 13:57:36 -0700

    edit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,9 +11,9 @@ int main(void)
 	char wor[] = "_putchar";
 	int car;
 
-	for (car = 0, car < 8, car++)
+	for (car = 0; car < 8; car++)
 	{
-		_putchar(wor[]);
+		_putchar(&wor[]);
 	}
 	_putchar('\n');
 	return (0);

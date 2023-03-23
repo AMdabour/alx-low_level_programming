a36cfc893c8cb39428ad553a7eb0a55bdcb9d7e5
AMdabour <ah910905@gmail.com>
2023-03-23 07:16:29 -0700

    initial commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - check if char is uppercase letter or not
+ *
+ * @c: the character to check.
+ *
+ * Return: 1 if uppercase and 0 if not
+*/
+
+int _isupper(int c)
+{
+	if (isupper(c))
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_h
+#define MAIN_h
+
+#include <stdio.h>
+#include <ctype.h>
+
+int _putchar(char c);
+
+int _isupper(int c);
+
+
+
+
+
+#endif

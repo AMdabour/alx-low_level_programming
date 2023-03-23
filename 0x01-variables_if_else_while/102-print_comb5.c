d4acce19871d8cbf99c4e31292b6ea042047824b
AMdabour <ah910905@gmail.com>
2023-03-23 01:55:50 -0700

    edit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,11 +20,11 @@ int main(void)
 				putchar(' ');
 				putchar(d2 / 10 + '0');
 				putchar(d2 % 10 + '0');
-			}
-			if (d1 != 98 && d2 != 99)
-			{
-				putchar(',');
-				putchar(' ');
+				if (d1 != 98 && d2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}

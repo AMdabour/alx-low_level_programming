475f46db73ac31fb1908cfbcb1d3a86274c0c302
AMdabour <ah910905@gmail.com>
2023-03-26 07:46:47 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two integers and return the result
+ *
+ * @x: the first integer
+ *
+ * @y: the second integer
+ *
+ * Return: gives the result
+*/
+
+int add(int x, int y)
+{
+	return (x + y);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -25,5 +25,7 @@ void jack_bauer(void);
 
 void times_table(void);
 
+int add(int x, int y);
+
 
 #endif

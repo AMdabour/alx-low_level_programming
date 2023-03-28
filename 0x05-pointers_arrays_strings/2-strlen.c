6185fef52480f6b7465336830d11a00c4a691924
AMdabour <ah910905@gmail.com>
2023-03-28 04:19:12 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - print the length of a string
+ *
+ * @s: the string
+ *
+ * Return: the length of the string
+*/
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}

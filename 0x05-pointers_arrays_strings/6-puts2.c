a9eeec818e628e258501d76499f4bac8419e5112
AMdabour <ah910905@gmail.com>
2023-03-29 05:29:19 -0700

    edit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,12 +10,11 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	for (; str[i] != '\0'; i++)
 	{
 		if ((i % 2) == 0)
 		{
 			_putchar(str[i]);
-			i++;
 		}
 		else
 			continue;

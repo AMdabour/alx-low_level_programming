b9db5c94b25c2fd3118eba32d552e62b253d1a83
AMdabour <ah910905@gmail.com>
2023-03-29 16:45:47 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenate two strings according to n value
+ *
+ * @dest: the first string
+ *
+ * @src: the second string
+ *
+ * @n: number of characters to be added
+ *
+ * Return: dest after concatenation
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j;
+
+	char *s;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	s = dest + i;
+
+	for (j = 0; j < n; j++)
+	{
+		*s++ = *src++;
+	}
+
+	return (dest);
+}

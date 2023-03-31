139f7ea607078a66ea1b267075a9555c667d466d
AMdabour <ah910905@gmail.com>
2023-03-31 10:33:11 -0700

    edit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -20,27 +20,27 @@ char *leet(char *c)
 		{
 			if (c[i] == 'A' || c[i] == 'a')
 			{
-				c[i] = *p[0];
+				c[i] = p[0][j];
 				break;
 			}
 			else if (c[i] == 'E' || c[i] == 'e')
 			{
-				c[i] = *p[1];
+				c[i] = p[1][j];
 				break;
 			}
 			else if (c[i] == 'O' || c[i] == 'o')
 			{
-				c[i] = *p[2];
+				c[i] = p[2][j];
 				break;
 			}
 			else if (c[i] == 'T' || c[i] == 't')
 			{
-				c[i] = *p[3];
+				c[i] = p[3][j];
 				break;
 			}
 			else if (c[i] == 'L' || c[i] == 'l')
 			{
-				c[i] = *p[4];
+				c[i] = p[4][j];
 				break;
 			}
 		}

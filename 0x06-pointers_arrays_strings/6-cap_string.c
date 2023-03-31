939cc3776b838b294d9455ac195b00d60df5f0fe
AMdabour <ah910905@gmail.com>
2023-03-31 05:11:58 -0700

    capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize every word
+ *
+ * @c: the string
+ *
+ * Return: the string after capitalization
+*/
+
+char *cap_string(char *c)
+{
+	int i;
+
+	if (c[0] >= 'a' && c[0] <= 'z')
+	{
+		c[0] -= 32;
+	}
+
+	for (i = 0; i < strlen(c); i++)
+	{
+		if (c[i] >= 32 && c[i] <= 34 || c[i] == '\n'
+		|| c[i] == 40 || c[i] == 41 || c[i] == 44 ||
+		c[i] == 46 || c[i] == 59 || c[i] == 63 ||
+		c[i] == 123 || c[i] == 125 || c[i] == '	')
+			if (c[i + 1] >= 'a' && c[i + 1] <= 'z')
+			{
+				c[i + 1] -= 32;
+			}
+			else
+			{
+				continue;
+			}
+		else
+		{
+			continue;
+		}
+	}
+
+	return (c);
+}

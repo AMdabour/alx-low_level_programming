2f580b60561449e104a6636c9d4bf597c8edea7e
AMdabour <ah910905@gmail.com>
2023-04-03 07:21:05 -0700

    edit spaces

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -21,6 +21,7 @@ int _atoi(char *s)
 	{
 		j++;
 	}
+
 	for (; i < j && d == 0; i++)
 	{
 		if (s[i] == '+')
@@ -34,11 +35,13 @@ int _atoi(char *s)
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			f = s[i] + '0';
+
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 			{
 				d = 1;
 			}
 		}
+
 		num = num * 10 + f;
 	}
 

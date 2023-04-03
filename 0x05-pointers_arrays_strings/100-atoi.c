db274b29c240f274ce5bf8f3a9bbd4ef10c9b12d
AMdabour <ah910905@gmail.com>
2023-04-02 23:51:29 -0700

    edit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,13 +10,18 @@
 
 int _atoi(char *s)
 {
-	unsigned long int i = 0;
+	int i = 0;
+	int j = 0;
 	int num = 0;
 	int f = 0;
 	int d = 0;
 	int sign = 0;
 
-	for (; i < strlen(s) && d == 0; i++)
+	while (s[j] != '\0')
+	{
+		j++;
+	}
+	for (; i < j && d == 0; i++)
 	{
 		if (s[i] == '+')
 		{
@@ -26,20 +31,16 @@ int _atoi(char *s)
 		{
 			sign--;
 		}
-
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			f = s[i] + '0';
-
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 			{
 				d = 1;
 			}
 		}
-
 		num = num * 10 + f;
 	}
-
 	if (sign >= 0)
 	{
 		sign = 1;
@@ -48,6 +49,5 @@ int _atoi(char *s)
 	{
 		sign = -1;
 	}
-
 	return (sign * num);
 }

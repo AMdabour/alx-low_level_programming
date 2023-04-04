fc9e28dee610345a3ab209d98b82ab6bdd8eceff
AMdabour <ah910905@gmail.com>
2023-04-03 21:27:33 -0700

    edit with *p

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -16,10 +16,12 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
+	char *p = s;
+
 	for (i = 0; i < n; i++)
 	{
-		*(s + i) = b;
+		*p++ = b;
 	}
 
-	return (s);
+	return (p);
 }

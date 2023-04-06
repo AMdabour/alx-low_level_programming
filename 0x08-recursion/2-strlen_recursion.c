96d2fbb13d638b0d0d394fffb880ec54dc0e9f91
AMdabour <ah910905@gmail.com>
2023-04-06 04:07:52 -0700

    edit

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,14 +10,12 @@
 
 int _strlen_recursion(char *s)
 {
-	int i = 0;
-
 	if (*s != '\0')
 	{
-		i++;
-		s++;
-		_strlen_recursion(s);
+		return (1 + _strlen_recursion(s + 1);
 	}
-
-	return (i);
+	else
+	{
+		return (0);
+	}
 }

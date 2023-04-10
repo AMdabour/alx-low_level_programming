067c0d7c0e5d3d00ca52fac815eb05d54e685895
AMdabour <ah910905@gmail.com>
2023-04-10 11:22:49 -0700

    arguments count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * @argc: argument count
+ *
+ * @argv: array of strings
+ *
+ * Return: 0 (success)
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}

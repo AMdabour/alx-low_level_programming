4e49ef7383ce1e59c3d23f644c150caede7501fa
AMdabour <ah910905@gmail.com>
2023-04-10 10:45:18 -0700

    initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * @argc: the number of arguments passed to main function
+ *
+ * @argv: array of strings
+ *
+ * Return: 0 (success)
+*/
+
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}

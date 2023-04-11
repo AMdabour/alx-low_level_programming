011fc88d5279694243bfb02694f31cf47a776c0e
AMdabour <ah910905@gmail.com>
2023-04-11 14:24:54 -0700

    _strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strdup - eturns a pointer to a copy of a given string
+ * @str: the string to be copied
+ * Return: the copy of the string
+*/
+
+char *_strdup(char *str)
+{
+	unsigned int i;
+
+	char *ptr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	ptr = (char *) malloc(l * sizeof(char));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < strlen(str); i++)
+	{
+		*(ptr + i) = *(str + i);
+	}
+
+	return (ptr);
+}

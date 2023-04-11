868a421a260e9afc9fb0437e42990546043410ed
AMdabour <ah910905@gmail.com>
2023-04-11 13:44:22 -0700

    initial commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars
+ * @size: the size of the array
+ * @c:a character
+ * Return: a pointer to  to the array on success
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+
+	ptr = (char *) malloc(size * sizeof(char));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		ptr + i = c;
+	}
+
+	return (ptr);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_h
+#define MAIN_h
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+
+#endif

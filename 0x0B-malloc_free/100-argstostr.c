ecefee3b73c0f957baafef08a8eb762ee1b35f88
AMdabour <ah910905@gmail.com>
2023-04-11 20:21:31 -0700

    argstostr

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * argstostr - turn all argumebts to string
+ * @ac: argumebt count
+ * @av: argument vector
+ * Return: string on success
+*/
+
+char *argstostr(int ac, char **av)
+{
+	int i, j;
+
+	char **ptr;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	ptr = (char **) malloc(ac * sizeof(char *));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		ptr[i] = (char *) malloc(strlen(av[i]) * sizeof(char));
+		if (ptr[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(ptr[j]);
+			}
+			free(ptr);
+
+			return (NULL);
+		}
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; j < strlen(av[i]); j++)
+		{
+			ptr[i][j] = av[i][j];
+		}
+		ptr[i][j] = '\n';
+	}
+
+	return (ptr);
+}

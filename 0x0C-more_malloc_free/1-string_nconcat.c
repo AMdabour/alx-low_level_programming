def14dfc6225413ae91e681ab98a5b53c563e370
AMdabour <ah910905@gmail.com>
2023-04-13 13:02:08 -0700

    edit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,46 +8,23 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1 = strlen(s1), len2 = strlen(s2), i;
+	unsigned int len1, len2, len;
 
-	char *ptr, *tmp;
+	char *ptr;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (n >= len2)
-	{
-		ptr = (char *) malloc(sizeof(char) * (len1 + len2 + 1));
-		if (ptr == NULL)
-			return (NULL);
-		tmp = ptr;
-		for (i = 0; i < (len1 + len2 + 1); i++)
-		{
-			if (*s1 != '\0')
-				*tmp++ = *s1++;
-			else
-				*tmp++ = *s2++;
-		}
-		*tmp = '\0';
-
-		return (ptr);
-	}
-	else
-	{
-		ptr = (char *) malloc(sizeof(char) * (len1 + n + 1));
-		if (ptr == NULL)
-			return (NULL);
-		tmp = ptr;
-		for (i = 0; i < (len1 + n + 1); i++)
-		{
-			if (*s1 != '\0')
-				*tmp++ = *s1++;
-			else
-				*tmp++ = *s2++;
-		}
-		*tmp = '\0';
-	}
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	len = len1 + (n < len2 ? n : len2);
+	ptr = (char *) malloc(sizeof(char) * (len + 1));
+	if (ptr == NULL)
+		return (NULL);
+	strncpy(ptr, s1, len1);
+	strncat(ptr, s2, (n < len2 ? n : len2));
+	ptr[len - 1] = '\0';
 
 	return (ptr);
 }

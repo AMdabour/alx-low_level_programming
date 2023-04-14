b102a1fcf1ce5deb4ff80128dacfd7095f155bcb
AMdabour <ah910905@gmail.com>
2023-04-14 06:29:46 -0700

    _realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * _realloc - reallocate memory for previously allocated one
+ * @ptr: pointer to the previously allocated space
+ * @old_size: the old space
+ * @new_size: the new space should be allocated
+ * Return: pointer to  the newly allocated memory
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *nptr;
+
+	if (ptr == NULL)
+	{
+		nptr = malloc(new_size);
+		if (nptr == NULL)
+			return (NULL);
+		return (nptr);
+	}
+	if (new_size <= old_size)
+	{
+		return (ptr);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+
+		return (NULL);
+	}
+	nptr = malloc(new_size);
+	if (nptr == NULL)
+		return (NULL);
+
+	memcpy(nptr, ptr, old_size);
+	free(ptr);
+
+	return (nptr);
+}

4426a258518f23530b32b1e57d88405d03587698
AMdabour <ah910905@gmail.com>
2023-04-17 02:28:15 -0700

    initial commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure named dog
+ * @name: first member
+ * @owner: second member
+ * @age: third member
+ *
+ * Description: structure containing three members
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif

ebdb4785659ba45e393780380f4dfdc4cc80014b
AMdabour <ah910905@gmail.com>
2023-04-17 03:23:07 -0700

    edit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,20 +6,12 @@
  * @name: name
  * @age: age
  * @owner: owner argument
+ *
+ * Description: initialize struct dog with the provided values
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	/**
-	 * struct dog *d - variable of type struct dog
-	 * @name: first member
-	 * @owner: second member
-	 * @age: third member
-	 * Description: variable of type struct dog conta.. three members
-	 */
-
-	struct dog *d;
-
 	d->name = name;
 	d->owner = owner;
 	d->age = age;

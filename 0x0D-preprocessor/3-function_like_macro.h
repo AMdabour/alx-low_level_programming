3dbb37a0722dc56a0e3230f4596fe92f91561ba0
AMdabour <ah910905@gmail.com>
2023-04-17 19:59:26 -0700

    ABS

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,11 +1,11 @@
 #ifndef ABS_H
 #define ABS_H
-#include <ctype.h>
 
 /**
  * ABS - a function like macro to compute the abs value of x
  * @x: the value to be absolutely computed
  */
+
 #define ABS(x) (x > 1 ? (1 * x) : (-1 * x))
 
 #endif

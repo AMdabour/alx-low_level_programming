04c8477506677b306e7ff83b55df9d24b702d02c
AMdabour <ah910905@gmail.com>
2023-04-19 05:16:43 -0700

    edit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,9 +6,7 @@
  * @f: a pointer to a function that prints the name
  */
 
-void print_name(char *name, void (*f)(char *))
+void print_name(char *name, void (*f)(char *s))
 {
-	if (name == NULL)
-		return;
 	(*f)(name);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 int _putchar(char c);
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *s));
 
 
 #endif

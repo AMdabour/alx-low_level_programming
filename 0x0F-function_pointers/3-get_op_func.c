b9fb6a2c790b3e82db726f3e9569db0f7cc3b065
AMdabour <ah910905@gmail.com>
2023-04-19 15:07:32 -0700

    edit

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -4,20 +4,20 @@
 /**
  * get_op_func - function pointer to select
  * @s: the operator
- * @a: first int
- * @b: second int
+ * @: first int
+ * @: second int
  * Return: result
  */
 
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
 	};
 
 	int i = 0;

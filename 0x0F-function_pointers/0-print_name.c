407fd6648645d8cfee0a881661dd9f13df6716be
AMdabour <ah910905@gmail.com>
2023-04-19 05:07:40 -0700

    initial commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - a function to print a name
+ * @name: the name to be printed
+ * @f: a pointer to a function that prints the name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+#ifndef FPOINTERS_H
+#define FPOINTERS_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+
+#endif

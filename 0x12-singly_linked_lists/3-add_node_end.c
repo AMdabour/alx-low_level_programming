d4cb4887c37c4cd2c04e5fb6f5a8193b8f8c5ad9
AMdabour <ah910905@gmail.com>
2023-04-27 12:53:20 -0700

    edit

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -27,7 +27,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	{
 		list_t *current = *head;
 
-		while (current != NULL)
+		while (current->next != NULL)
 		{
 			current = current->next;
 		}

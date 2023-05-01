966db19792e9b3484d5046a0e938dda205a9a748
AMdabour <ah910905@gmail.com>
2023-04-30 18:07:00 -0700

    free list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -10,12 +10,14 @@ void free_listint(listint_t *head)
 {
 	listint_t *current = head;
 
-	while (current->next != NULL)
+	while (current != NULL)
 	{
 		listint_t *next = current->next;
 
-		free(current->n);
+		free(&(current->n));
 		free(current);
 		current = next;
 	}
+
+	free(current);
 }

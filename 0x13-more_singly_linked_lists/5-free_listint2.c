246e552893d4bdf67fa2f5ee80018e656011ed0a
AMdabour <ah910905@gmail.com>
2023-05-02 09:00:36 -0700

    free2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - free a list
+ * @head: the case list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+
+	while (current != NULL)
+	{
+		listint_t *next = current->next;
+
+		free(current);
+		current = next;
+	}
+
+	free(current);
+	head = NULL;
+}

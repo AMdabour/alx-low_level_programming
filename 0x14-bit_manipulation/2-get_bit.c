ff92898c5532f0efc6d68b40ec214f10170874a3
AMdabour <ah910905@gmail.com>
2023-05-04 16:16:10 -0700

    edit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -14,9 +14,6 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (index >= sizeof(n) * 8)
 		return (-1);
 
-	if (n == 0 && index == 0)
-		return (0);
-
 	for (i = sizeof(n) * 8 - 1; i > 0; i--)
 	{
 		if ((n >> i & 1))

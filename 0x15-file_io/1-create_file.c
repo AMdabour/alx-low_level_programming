58dd6953bb7159280c5598f8dc2f73e32591270a
AMdabour <ah910905@gmail.com>
2023-05-09 09:12:27 -0700

    edit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,19 +16,19 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+
 	fd = open(filename, O_CREAT |
 		O_RDWR | O_TRUNC | 0600);
-	if (fd == -1)
-		return (-1);
+
 	if (text_content != NULL)
 	{
 		len = strlen(text_content);
-		write_return = write(fd, text_content, len);
-		if (write_return == -1)
-		{
-			return (-1);
-		}
 	}
+
+	write_return = write(fd, text_content, len);
+
+	if (fd == -1 || write_return == -1)
+		return (-1);
 
 	close(fd);
 

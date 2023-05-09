2749bcd0fc3db3c8c5f9af562c3f93401adc416a
AMdabour <ah910905@gmail.com>
2023-05-09 04:46:44 -0700

    create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+
+/**
+ * create_file - create a file
+ * @filename: the name of the file
+ * @text_content: content of the file
+ * Return: 1 on success -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len, write_return;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_RDWR |
+		O_CREAT | O_TRUNC | S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		len = strlen(text_content);
+		write_return = write(fd, text_content, len);
+		if (write_return == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+
+	return (1);
+}

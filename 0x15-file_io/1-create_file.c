d5276bc1874f4daebc8b43203db60b893bfecc1c
AMdabour <ah910905@gmail.com>
2023-05-09 09:15:55 -0700

    edit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,13 +17,13 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT |
-		O_RDWR | O_TRUNC | 0600);
-
 	if (text_content != NULL)
 	{
 		len = strlen(text_content);
 	}
+
+	fd = open(filename, O_CREAT |
+		O_RDWR | O_TRUNC | 0600);
 
 	write_return = write(fd, text_content, len);
 

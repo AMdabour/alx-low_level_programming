65e4821b56f7c32b9f472411e01b4f5704003be7
AMdabour <ah910905@gmail.com>
2023-06-15 05:12:36 -0700

    num of elements in a list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the num of elements in a list
+ * @h: the list to check
+ * Return: num of elements in the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i = 0;
+	dlistint_t *tmp = h;
+
+	while (tmp)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+
+	return (i);
+}

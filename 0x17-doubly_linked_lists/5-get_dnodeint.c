3cdbd4f100a6e989ea2f98f2fc7e5cd27296f620
AMdabour <ah910905@gmail.com>
2023-06-15 07:50:37 -0700

    node at an index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - get a node in a given index
+ * @head: the list to check
+ * @index: the index of the node
+ * Return: tmp->n or null
+ * */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *tmp = head, *tmp2 = head;
+	size_t i, j = 0;
+
+	if (!head)
+		return (NULL);
+
+	while (tmp2)
+	{
+		tmp2 = tmp2->next;
+		j++;
+	}
+
+	if (j >= index)
+	{
+		for (i = 0; i < index; i++)
+		{
+			tmp = tmp->next;
+		}
+
+		return (tmp);
+	}
+
+	return (NULL);
+}

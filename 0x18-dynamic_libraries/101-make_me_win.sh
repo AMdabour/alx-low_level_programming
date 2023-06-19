eb59bac5d3d482324469299710e1cdfbfc8e8a34
AMdabour <ah910905@gmail.com>
2023-06-19 13:22:30 -0700

    edit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p /tmp https://github.com/AMdabour/alx-low_level_programming/blob/master/0x18-dynamic_libraries/test.so
-export LD_PRELOAD=/tmp/test.so
+wget -p /tmp https://github.com/AMdabour/alx-low_level_programming/blob/master/0x18-dynamic_libraries/hacked.so
+export LD_PRELOAD=/tmp/hacked.so
